7364397b05e6535960d7c864ccdb7b50faa571ac
Aditya Patwardhan <adibpat@Adityas-MacBook-Pro.local>
2017-06-06 18:39:16 -0400

    3

--- hello_world.py
+++ hello_world.py
@@ -8,8 +8,16 @@ print("Hello, World")
 def print_hello(who):
     print("hello, {}".format(who))
 
+# int, float, null and bool are basic data types in python
 
 
 if __name__ == "__main__":
-    print_hello("Sharda")
+
     print_hello(sys.argv[1])
+
+    print(0x10) #16 
+    print(0b10) #2
+    print(0o10) #8
+    print(int(-4.4999)) #-4
+    print(type(3.45)) #float
+    
